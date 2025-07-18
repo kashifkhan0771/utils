f774613c019d7d48bfbbfee271dbd5abc34f709c
Danilo Sindric <danilosindric@gmail.com>
2025-07-18 16:45:56 +0200

    Update: Loading image from URL

--- image/image.go
+++ image/image.go
@@ -6,6 +6,9 @@ import (
 	"image/color"
 	"image/jpeg"
 	"image/png"
+	"io"
+	"mime"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -23,8 +26,11 @@ type Image struct {
 type ImageFormat string
 
 const (
-	FormatJPG ImageFormat = "jpg"
-	FormatPNG ImageFormat = "png"
+	FormatJPG  ImageFormat = "jpg"
+	FormatJFIF ImageFormat = "jfif"
+	FormatJPE  ImageFormat = "jpe"
+	FormatJPEG ImageFormat = "jpeg"
+	FormatPNG  ImageFormat = "png"
 )
 
 func LoadFromFile(path string) (*Image, error) {
@@ -35,27 +41,57 @@ func LoadFromFile(path string) (*Image, error) {
 	}
 	defer f.Close()
 
-	var img image.Image
 	format := ImageFormat(filepath.Ext(path)[1:])
-
-	switch format {
-	case FormatJPG:
-		img, err = jpeg.Decode(f)
-	case FormatPNG:
-		img, err = png.Decode(f)
-	default:
-		return nil, fmt.Errorf("invalid or unsupported image format: %s", format)
-	}
-
+	img, err := decodeImage(format, f)
 	if err != nil {
 		return nil, err
 	}
 
+	return new(format, img), nil
+}
+
+func LoadFromURL(url string) (*Image, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	extensions, err := mime.ExtensionsByType(resp.Header.Get("Content-Type"))
+	if err != nil {
+		return nil, err
+	}
+
+	if len(extensions) == 0 {
+		return nil, fmt.Errorf("unable to determine image format: no file extensions found for Content-Type '%s'", resp.Header.Get("Content-Type"))
+	}
+
+	format := ImageFormat(extensions[0][1:])
+	img, err := decodeImage(format, resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return new(format, img), nil
+}
+
+func decodeImage(format ImageFormat, r io.Reader) (image.Image, error) {
+	switch format {
+	case FormatJPG, FormatJPEG, FormatJFIF, FormatJPE:
+		return jpeg.Decode(r)
+	case FormatPNG:
+		return png.Decode(r)
+	default:
+		return nil, fmt.Errorf("invalid or unsupported image format: %s", format)
+	}
+}
+
+func new(format ImageFormat, img image.Image) *Image {
 	return &Image{
 		Image:      img,
 		Format:     format,
 		Width:      img.Bounds().Max.X - img.Bounds().Min.X,
 		Height:     img.Bounds().Max.Y - img.Bounds().Min.Y,
 		ColorModel: img.ColorModel(),
-	}, nil
+	}
 }
