5d8b72a975954a2e2032344525aa8bc08c510af4
Kenneth Mebarkia Thomsen <kenneth@addvanced.dk>
2025-08-06 07:05:57 +0200

    Some minor refactoring

--- fake/fake.go
+++ fake/fake.go
@@ -4,7 +4,8 @@ Package fake provides utilities for generating fake data.
 package fake
 
 import (
-	"crypto/rand"
+	cryptrnd "crypto/rand"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"time"
@@ -32,9 +33,9 @@ var postalCodes = []string{"12345", "23456", "34567", "45678", "56789", "67890",
 // RandomUUID generates a fake UUIDv4.
 func RandomUUID() (string, error) {
 	uuid := make([]byte, 16)
-	_, err := io.ReadFull(rand.Reader, uuid)
+	_, err := io.ReadFull(cryptrnd.Reader, uuid)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate random UUID: %w", err)
 	}
 
 	// Set the version to 4
@@ -42,77 +43,70 @@ func RandomUUID() (string, error) {
 	// Set the variant to 2 (RFC 4122)
 	uuid[8] = (uuid[8] & 0x3f) | 0x80
 
-	return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x", uuid[:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
+	uuidStr := hex.EncodeToString(uuid)
+
+	return uuidStr[0:8] + "-" + uuidStr[8:12] + "-" + uuidStr[12:16] + "-" + uuidStr[16:20] + "-" + uuidStr[20:32], nil
 }
 
 // RandomDate generates a random date.
 func RandomDate() (time.Time, error) {
-	start := time.Date(EpochYear, time.Month(EpochMonth), EpochDay, EpochHour, EpochMinute, EpochSecond, EpochNano, time.UTC).Unix()
-	end := time.Now().Unix()
+	start := time.Date(EpochYear, time.Month(EpochMonth), EpochDay, EpochHour, EpochMinute, EpochSecond, EpochNano, time.UTC).UnixNano()
+	end := time.Now().UnixNano()
 
-	sec, err := rnd.NumberInRange(start, end)
+	nanos, err := rnd.NumberInRange(start, end)
 	if err != nil {
-		return time.Time{}, err
+		return time.Time{}, fmt.Errorf("failed to generate random time: %w", err)
 	}
 
-	nsec, err := rnd.NumberInRange(0, 1e9)
-	if err != nil {
-		return time.Time{}, err
-	}
-
-	return time.Unix(sec, nsec), nil
+	return time.Unix(0, nanos), nil
 }
 
 // RandomPhoneNumber generates a random phone number.
 func RandomPhoneNumber() (string, error) {
 	areaCode, err := rnd.NumberInRange(100, 999)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate areaCode: %w", err)
 	}
 
 	firstPart, err := rnd.NumberInRange(100, 999)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate firstPart: %w", err)
 	}
 
 	secondPart, err := rnd.NumberInRange(1000, 9999)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate secondPart: %w", err)
 	}
 
-	return fmt.Sprintf("+1 (%d) %d-%d", areaCode, firstPart, secondPart), nil
+	return fmt.Sprintf("+1 (%03d) %03d-%04d", areaCode, firstPart, secondPart), nil
 }
 
 // RandomAddress generates a random address.
 func RandomAddress() (string, error) {
-	streetNumber, err := rnd.NumberInRange(0, MaxStreetNumber)
+	streetNumber, err := rnd.NumberInRange(1, MaxStreetNumber)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to generate street number: %w", err)
 	}
 
-	idx, err := rnd.NumberInRange(0, int64(len(streetNames)-1))
+	streetName, err := rnd.Pick(streetNames)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to pick street name: %w", err)
 	}
-	streetName := streetNames[idx]
 
-	idx, err = rnd.NumberInRange(0, int64(len(streetNames)-1))
+	city, err := rnd.Pick(cities)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to pick city: %w", err)
 	}
-	city := cities[idx]
 
-	idx, err = rnd.NumberInRange(0, int64(len(states)-1))
+	state, err := rnd.Pick(states)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to pick state: %w", err)
 	}
-	state := states[idx]
 
-	idx, err = rnd.NumberInRange(0, int64(len(postalCodes)-1))
+	postalCode, err := rnd.Pick(postalCodes)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to pick postal code: %w", err)
 	}
-	postalCode := postalCodes[idx]
 
 	return fmt.Sprintf("%d %s, %s, %s %s, USA", streetNumber, streetName, city, state, postalCode), nil
 }
